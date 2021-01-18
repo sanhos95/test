07fc3489cfcbb2ec4adfbfd4bf721e6574196e0a
Ivan.PS <sanhos.95@gmail.com>
2021-01-18 10:48:07 +0200

    Öå ïåðøèé êîìåíò

---
+++ Site 1/script.js
@@ -0,0 +1,10 @@
+
+var
+money = ("Ваш бюджет на месяц?"),
+time = ("Введите дату в формате YYYY-MM-DD"); 
+
+// alert(money);
+// alert(time);
+let answer = +prompt("Есть ли вам 18?", "да");
+console.log(answer);
+
